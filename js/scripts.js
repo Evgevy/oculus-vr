aa8676eeba5cccb47821943e85d3e8cc33c0542c
Evgeny <djek.uait1998@gmail.com>
2023-04-15 00:54:05 +0300

    some commit

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,51 @@ $(document).ready(function (){
 })
 
 
+$(document).ready(function(){
+    $(".more__items").slick({
+        prevArrow:".more__button-prev",
+        nextArrow:".more__button-next",
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1281,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2
+              }
+            },
 
+            {
+              breakpoint: 1001,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+
+            {
+              breakpoint: 706,
+              settings: {
+                prevArrow: false,
+                nextArrow: false,
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 607,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+
+    })
+});
 $(document).ready(function (){
     $(".popup__close").click(function(){
         $(".popup").toggleClass("popup--hide")
